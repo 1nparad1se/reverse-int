9dc878aa5838b4aeeb78f898c0c68bc42b09c312
Jeyhun <inparadisejeyhun@gmail.com>
2024-08-05 02:00:38 +0300

    feat: new func reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let total = '';
+    n = n.toString();
+    for (let i = 0; i < n.length; ++i) {
+        if (n[i] === '-') {
+            total = total
+        } else {
+            total = `${n[i]}${total}`
+        }
+    }
+    return Number(total);
 }
